492930fbeaf332457580cf9f22f372e39c5e44b1
asukwoanso <asukwoanso2018@gmail.com>
2024-02-27 21:31:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+Javascript_repo
+//data type
+//number
+console.log(10 * 5);
+console.log(Math.PI);
+console.log(+"10");
+console.log(isNaN("Asukwo"));
+
+//String 
+//sequences of unicode character
+console.log("Hello");
+console.log("super duper".length);
+console.log("chris".charAt(4));
+console.log("Hello"+""+"World"+"!");
+console.log("I want to be uppercase!!!".toUpperCase());
+
+//Boolean
+var shouldBeTrue =false;
+if (shouldBeTrue === true){
+    console.log("Hello");
+}
+else{
+    console.log("This var is not true");
+}
+//the following evaluation to true
+var hascontent = "asukwoanso";
+
+//the following evaluation to false
+var doesNotHaveContent = "";
+var isZero = 0;
+var nonAssignedVariable;
+console.log(typeof nonAssignedVariable);
+console.log(Boolean(nonAssignedVariable));
